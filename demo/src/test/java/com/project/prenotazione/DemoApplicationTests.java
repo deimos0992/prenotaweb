a1e3b836dcd6f0a8323a1c96612ea4f7673b8e18
deimos0992 <luigi.benvenuto01@gmail.com>
2021-01-05 17:20:01 +0100

    first commit

---
+++ demo/src/test/java/com/project/prenotazione/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prenotazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
