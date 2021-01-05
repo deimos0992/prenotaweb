ae2ce8c69154d162b5cf5745b3e7b3f838f49603
deimos0992 <luigi.benvenuto01@gmail.com>
2021-01-05 18:12:21 +0100

    boilerplate preonaweb - Connessione al DB - Aggiunta Swagger

--- demo/src/main/java/com/project/prenotazione/DemoApplication.java
+++ demo/src/main/java/com/project/prenotazione/DemoApplication.java
@@ -2,8 +2,10 @@ package com.project.prenotazione;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
